47345c8b56e5ff1f19e04145d556faec830645e4
Ryan <straightwind0726@gmail.com>
2023-08-30 22:03:39 +0900

    [Update] Project is Done

--- src/Components/CategoryBox.jsx
+++ src/Components/CategoryBox.jsx
@@ -1,6 +1,7 @@
-import { useRecoilState, useRecoilValue } from 'recoil';
+import { useRecoilState } from 'recoil';
 import { styled } from 'styled-components';
 import { categories, currentCategory } from '../atom';
+import { FaTrash } from 'react-icons/fa';
 
 const CategoryBtn = styled.button`
     color: var(--text-100);
@@ -12,9 +13,18 @@ const CategoryBtn = styled.button`
     transition: 300ms all;
 `;
 
+const CategoryDelete = styled.button`
+    color: var(--text-100);
+    font-size: 18px;
+    transition: 300ms all;
+    &:hover {
+        transform: scale(1.15);
+    }
+`;
+
 export default function CategoryBox() {
     const [cat, setCat] = useRecoilState(currentCategory);
-    const catArr = useRecoilValue(categories);
+    const [catArr, setCatArr] = useRecoilState(categories);
 
     const toggleCategory = (category) => {
         if (cat === category) {
@@ -23,18 +33,35 @@ export default function CategoryBox() {
         setCat(category);
     };
 
+    const deleteCategory = (category) => {
+        const newCat = catArr.filter((a) => a !== category);
+        setCatArr(newCat);
+    };
+
     return (
-        <div>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
             {catArr.map((category) => {
                 return (
-                    <CategoryBtn
-                        onClick={() => toggleCategory(category)}
-                        style={{
-                            color: cat === category ? '#ff3d3d' : '#fff',
-                        }}
-                    >
-                        {category}
-                    </CategoryBtn>
+                    <div>
+                        <CategoryBtn
+                            onClick={() => toggleCategory(category)}
+                            style={{
+                                color: cat === category ? '#ff3d3d' : '#fff',
+                            }}
+                        >
+                            {category}
+                        </CategoryBtn>
+                        <CategoryDelete
+                            onClick={() => {
+                                deleteCategory(category);
+                            }}
+                            style={{
+                                color: cat === category ? '#ff3d3d' : '#fff',
+                            }}
+                        >
+                            <FaTrash />
+                        </CategoryDelete>
+                    </div>
                 );
             })}
         </div>
